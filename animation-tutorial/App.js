23a7a8353806ac91c2be7cb30ba076f4904a3af7
ASWIN K MANOJ <aswinkmanoj101@gmail.com>
2023-07-14 01:04:57 +0530

    Pan, Pinch , Tap

--- animation-tutorial/App.js
+++ animation-tutorial/App.js
@@ -1,20 +1,21 @@
+import { useState } from 'react';
 import { Dimensions } from 'react-native';
 import { StyleSheet, View } from 'react-native';
-import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
-import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import { GestureHandlerRootView, PanGestureHandler, PinchGestureHandler, TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, { interpolateColor, runOnJS, useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 
-const { width: WIDTH } = Dimensions.get('window')
+const { width: WIDTH } = Dimensions.get('window');
 
 export default function App() {
 
   const tranX = useSharedValue(0);
   const tranY = useSharedValue(0);
-  const scaled = useSharedValue(1);
+  const Panscaled = useSharedValue(1);
 
 
   const onGesturEvent = useAnimatedGestureHandler({
     onStart: () => {
-      scaled.value = withSpring(1.25);
+      Panscaled.value = withSpring(1.25);
     },
     onActive: ({ translationX, translationY }) => {
       tranX.value = translationX;
@@ -23,20 +24,67 @@ export default function App() {
     onFinish: () => {
       tranX.value = withSpring(0);
       tranY.value = withSpring(0);
-      scaled.value = withSpring(1);
+      Panscaled.value = withSpring(1);
     }
   })
 
+  const Scale = useSharedValue(1);
+  const context = useSharedValue(1);
+
+  const Pinch = useAnimatedGestureHandler({
+    onStart: (event) => {
+      context.value = event.scale;
+    },
+    onActive: ({ scale }) => {
+      Scale.value = context.value + scale
+    },
+    onEnd: () => {
+      Scale.value = 1;
+    }
+  })
+
+  const [first, setFirst] = useState(true);
+  const r = useSharedValue(1);
+
+  const Tap = useAnimatedGestureHandler({
+    onActive: () => {
+      runOnJS(setFirst)(!first)
+      r.value = first ? withTiming(1) : withTiming(0);
+      console.log(first);
+    }
+  })
+
+  const c = useAnimatedStyle(() => ({
+    backgroundColor: first ? interpolateColor(
+      r.value,
+      [0, 1],
+      ['red', 'green']
+    ) : interpolateColor(
+      r.value,
+      [1, 0],
+      ['green', 'red']
+    )
+  }))
+  const pCircle = useAnimatedStyle(() => ({
+    transform: [{ scale: withSpring(Scale.value) }]
+  }))
+
   const rCircle = useAnimatedStyle(() => ({
-    transform: [{ translateX: tranX.value }, { translateY: tranY.value }, { scale: scaled.value }]
+    transform: [{ translateX: tranX.value }, { translateY: tranY.value }, { scale: Panscaled.value }]
   }))
 
   return (
     <GestureHandlerRootView style={styles.container}>
-      <View style={styles.container}>
+      <View style={[styles.container, { justifyContent: 'space-evenly' }]}>
         <PanGestureHandler onGestureEvent={onGesturEvent}>
           <Animated.View style={[styles.circle, rCircle,]} />
         </PanGestureHandler>
+        <PinchGestureHandler onGestureEvent={Pinch}>
+          <Animated.View style={[styles.circle, pCircle, { width: WIDTH * .5, height: WIDTH * .5, borderRadius: WIDTH * .5 }]} />
+        </PinchGestureHandler>
+        <TapGestureHandler numberOfTaps={2} onGestureEvent={Tap}>
+          <Animated.View style={[styles.circle, c]} />
+        </TapGestureHandler>
       </View>
     </GestureHandlerRootView>
   );
